362ac04db2d7e571e5a3c310b0691acecd330acc
Prasoon <prasoonjadon.pis6@gmail.com>
2025-09-13 17:49:45 +0530

    Create sw.js

---
+++ fintech/sw.js
@@ -0,0 +1,12 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('vyoma-debt-crusher-v1').then(cache => cache.addAll([
+      '/', '/index.html', '/manifest.json'
+    ]))
+  );
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request).then(r=>{ return caches.open('vyoma-debt-crusher-v1').then(cache=>{ cache.put(event.request, r.clone()); return r; }); }))
+  );
+});
